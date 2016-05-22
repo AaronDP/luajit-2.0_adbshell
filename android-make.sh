49580038ccd0833740c15bf2a42410a1d60202f4
Aaron D. Pierce <aaron.dennis.pierce@gmail.com>
2016-05-22 12:53:44 -0600

    Bash script to accomplish the build using NDK toolchain

---
+++ android-make.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+NDK=/opt/ndk
+NDKABI=14
+NDKVER=$NDK/toolchains/arm-linux-androideabi-4.9
+NDKP=$NDKVER/prebuilt/linux-x86/bin/arm-linux-androideabi-
+NDKF="--sysroot $NDK/platforms/android-$NDKABI/arch-arm"
+NDKARCH="-march=armv7-a -mfloat-abi=softfp -Wl,--fix-cortex-a8"
+make HOST_CC="gcc -m32" CROSS=$NDKP TARGET_FLAGS="$NDKF $NDKARCH"
